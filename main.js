c96468ea83ea1d2ec80294babc1a6700bbfa92d1
Megha Jain <mj2905@uw.edu>
2021-12-05 17:56:54 -0800

    modified chart

--- main.js
+++ main.js
@@ -1,152 +1,3 @@
-// // Global function called when select element is changed
-// function onCategoryChanged() {
-//     var select = d3.select('#categorySelect').node();
-//     // Get current value of select element
-//     var category = select.options[select.selectedIndex].value;
-//     // Update chart with the selected category of letters
-//     updateChart(category);
-// }
-
-// // recall that when data is loaded into memory, numbers are loaded as strings
-// // this function helps convert numbers into string during data preprocessing
-// function dataPreprocessor(row) {
-//     return {
-//         Type: row.Type,
-//         Occupation: row.Occupation,
-//         All_workers: +row.All_workers,
-//         All_weekly: +row.All_weekly,
-//         M_workers: +row.M_workers,
-//         M_weekly: +row.M_weekly,
-//         F_workers: +row.F_workers,
-//         F_weekly: +row.F_weekly
-//     };
-// }
-
-// var svg = d3.select('svg');
-
-// // Get layout parameters
-// var svgWidth = +svg.attr('width');
-// var svgHeight = +svg.attr('height');
-
-// var padding = {t: 60, r: 40, b: 30, l: 40};
-
-// // Compute chart dimensions
-// var chartWidth = svgWidth - padding.l - padding.r;
-// var chartHeight = svgHeight - padding.t - padding.b;
-
-// // Compute the spacing for bar bands based on all 26 letters
-// var barBand = chartHeight / 26;
-// var barHeight = barBand * 0.7;
-
-// // Create a group element for appending chart elements
-// var chartG = svg.append('g')
-//     .attr('transform', 'translate('+[padding.l, padding.t]+')');
-
-// // A map with arrays for each category of letter sets
-// var jobsMap = {
-//     'all-jobs': 'MANAGEMENT, BUSINESS, COMPUTATIONAL, ENGINEERING, SCIENCE, SOCIAL SERVICE, LEGAL, EDUCATION, ARTS, HEALTHCARE PROFESSIONAL, PROTECTIVE SERVICE, CULINARY, GROUNDSKEEPING, SERVICE, SALES, OFFICE, AGRICULTURAL, CONSTRUCTION, MAINTENANCE, PRODUCTION, TRANSPORTATION'.split(', '),
-//     'management': 'MANAGEMENT',
-//     'business': 'BUSINESS',
-//     'computational': 'COMPUTATIONAL',
-//     'engineering': 'ENGINEERING',
-//     'science': 'SCIENCE',
-//     'social service': 'SOCIAL SERVICE',
-//     'legal': 'LEGAL',
-//     'education': 'EDUCATION',
-//     'arts': 'ARTS',
-//     'healthcare': 'HEALTHCARE',
-//     'professional': 'PROFESSIONAL',
-//     'protective service': 'PROTECTIVE SERVICE',
-//     'culinary': 'CULINARY',
-//     'groundskeeping': 'GROUNDSKEEPING',
-//     'service': 'SERVICE',
-//     'sales': 'SALES',
-//     'office': 'OFFICE',
-//     'agricultural': 'AGRICULTURAL',
-//     'construction': 'CONSTRUCTION',
-//     'maintenance': 'MAINTENANCE',
-//     'production': 'PRODUCTION',
-//     'transportation': 'TRANSPORTATION'
-// };
-
-// d3.csv('inc_occ_gender.csv', dataPreprocessor).then(function(dataset) {
-//     // Create global variables here and intialize the chart
-//     incomes = dataset;
-//     console.log(incomes);
-
-//     // **** Your JavaScript code goes here ****
-//     widthScale = d3.scaleLinear()
-//         .domain([0, d3.max(incomes, function(d) { return d.All_weekly; })])
-//         .range([0, chartWidth]);
-
-//     var axisTop = d3.axisTop(widthScale).ticks(6).tickFormat(x => x * 100 + '%');
-//     var axisBottom = d3.axisBottom(widthScale).ticks(6).tickFormat(x => x * 100 + '%');
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(" + padding.r + "," + padding.t + ")")
-//         .attr("text-anchor", "start")
-//         .call(axisTop);
-
-//     svg.append("g")
-//         .attr("class", "x axis")
-//         .attr("transform", "translate(" + padding.r + "," + (svgHeight - padding.b) + ")")
-//         .attr("text-anchor", "start")
-//         .call(axisBottom);
-
-//     svg.append('text')
-//         .attr("x", (svgWidth / 2))
-//         .attr("y", (padding.t / 2))
-//         .attr("text-anchor", "middle")
-//         .style("font-size", "16px")
-//         .text('Weekly Wage ($)');
-
-//     // Update the chart for all letters to initialize
-//     updateChart('all-jobs');
-// });
-
-
-// function updateChart(filterKey) {
-//     var filteredJobs = incomes.filter(function(d){
-//         return jobsMap[filterKey].includes(d.type);
-//     });
-//     // Create a filtered array of letters based on the filterKey
-
-//     // **** Draw and Update your chart here ****
-//     var bars = chartG.selectAll('.bar')
-//     .data(filteredJobs, function(d) {
-//         return d.Occupation;
-//     })
-
-//     var barEnter = bars.enter()
-//         .append("g")
-//         .attr('class', 'bar');
-
-//         barEnter.merge(bars)
-//         .attr('transform', function(d, i) {
-//             return 'translate(' + [0, i * barBand + 4] + ')';
-//         })
-
-//         barEnter.append('rect')
-//         .attr('width', function(d) {
-//             return xScale(d.All_weekly);
-//         })
-//         .attr('height', barHeight);
-
-//         barEnter.append('text')
-//         .attr('x', -20)
-//         .attr('dy', '0.93m')
-//         .text(function(d) {
-//             return d.Occupation;
-//         })
-//     bars.exit().remove();
-// }
-
-// Remember code outside of the data callback function will run before the data loads
-
-
-// NEW VERSION ---------------------------------------------------------
-
 // Global function called when select element is changed
 function onCategoryChanged() {
     var select = d3.select('#categorySelect').node();
@@ -177,15 +28,15 @@ var svg = d3.select('svg');
 var svgWidth = +svg.attr('width');
 var svgHeight = +svg.attr('height');
 
-var padding = {t: 60, r: 40, b: 30, l: 40};
+var padding = {t: 60, r: 40, b: 30, l: 400};
 
 // Compute chart dimensions
 var chartWidth = svgWidth - padding.l - padding.r;
 var chartHeight = svgHeight - padding.t - padding.b;
 
-// Compute the spacing for bar bands based on all 26 letters
-var barBand = chartHeight / 26;
-var barHeight = barBand * 0.7;
+// Compute the spacing for bar bands based on all 538 occupations
+var barBand = chartHeight / 538;
+var barHeight = barBand * .7;
 
 // Create a group element for appending chart elements
 var chartG = svg.append('g')
@@ -230,19 +81,19 @@ d3.csv('inc_occ_gender.csv', dataPreprocessor).then(function(dataset) {
     // **** Your JavaScript code goes here ****
 
     var xAxis = d3.axisTop(xScale).ticks(6).tickFormat(function(d) {
-        return (d * 100) + '%';
+        return '$' + (d);
     })
 
     var xAxisBottom = d3.axisBottom(xScale).ticks(6).tickFormat(function(d) {
-        return (d * 100) + '%';
+        return '$' + (d);
     })
 
     svg.append('g')
-    .attr("transform", "translate(42, 50)")
+    .attr("transform", "translate(400, 50)")
     .call(xAxis);
 
     svg.append('g')
-    .attr("transform", "translate(42, 570)")
+    .attr("transform", "translate(400, 9970)")
     .call(xAxisBottom);
 
     svg.append('text')
