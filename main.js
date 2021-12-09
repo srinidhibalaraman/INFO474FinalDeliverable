bd5e9454eb91d6942243c346f21512b41722e165
srinidhibalaraman <srinidhi.balaraman@gmail.com>
2021-12-08 21:43:37 -0800

    final push

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ var chartWidth = svgWidth - padding.l - padding.r;
 var chartHeight = svgHeight - padding.t - padding.b;
 
 // Compute the spacing for bar bands based on all 538 occupations
-var barBand = chartHeight / 523;
+var barBand = chartHeight / 112;
 var barHeight = barBand * .7;
 
 // Create a group element for appending chart elements
@@ -55,7 +55,6 @@ var jobsMap = {
     'education': 'EDUCATION',
     'arts': 'ARTS',
     'healthcare': 'HEALTHCARE',
-    'professional': 'PROFESSIONAL',
     'protective service': 'PROTECTIVE SERVICE',
     'culinary': 'CULINARY',
     'groundskeeping': 'GROUNDSKEEPING',
@@ -63,13 +62,11 @@ var jobsMap = {
     'sales': 'SALES',
     'office': 'OFFICE',
     'agricultural': 'AGRICULTURAL',
-    'construction': 'CONSTRUCTION',
-    'maintenance': 'MAINTENANCE',
     'production': 'PRODUCTION',
     'transportation': 'TRANSPORTATION'
 };
 
-d3.csv('inc_occ_gender.csv', dataPreprocessor).then(function(dataset) {
+d3.csv('occ_pay_by_gender.csv', dataPreprocessor).then(function(dataset) {
     // Create global variables here and intialize the chart
     incomes = dataset;
     console.log(incomes);
@@ -81,7 +78,7 @@ d3.csv('inc_occ_gender.csv', dataPreprocessor).then(function(dataset) {
     console.log(groups);
 
     xScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d.All_weekly; })])
+    .domain([0, d3.max(dataset, function(d) { return d.M_weekly; })])
     .range([0, chartWidth]);
 
     // **** Your JavaScript code goes here ****
@@ -102,13 +99,23 @@ d3.csv('inc_occ_gender.csv', dataPreprocessor).then(function(dataset) {
     .call(xAxis);
 
     svg.append('g')
-    .attr("transform", "translate(500, 9970)")
+    .attr("transform", "translate(500, 4170)")
     .call(xAxisBottom);
 
     svg.append('text')
-    .attr('x', 80)
+    .attr('x', 500)
     .attr('y', 20)
-    .text('Weekly Wage ($)');
+    .attr('fill', 'white')
+    .text('Weekly Wage ($1000)');
+
+    var legend = d3.select("#my_dataviz")
+
+// Handmade legend
+    legend.append("text").attr('x', 190).attr('y', 100).text('Legend').attr('font-size', '20px').attr('font-weight', 'bold').attr('fill', 'white');
+    legend.append("circle").attr("cx",170).attr("cy",130).attr("r", 6).style("fill", "#013F94")
+    legend.append("circle").attr("cx",170).attr("cy",160).attr("r", 6).style("fill", "#C7E6F9")
+    legend.append("text").attr("x", 190).attr("y", 130).text("Male Workers").style("font-size", "15px").attr("alignment-baseline","middle").attr('fill', 'white')
+    legend.append("text").attr("x", 190).attr("y", 160).text("Female Workers").style("font-size", "15px").attr("alignment-baseline","middle").attr('fill', 'white')
 
     //hoverChart(incomes);
 
@@ -139,6 +146,7 @@ function hoverChart(dataset) {
             .text(dataset[i]['name'])
             .attr('font-size', '10px')
             .attr('font-weight', 'bold')
+            .attr('fill', 'white');
     }
 }
 
@@ -169,27 +177,29 @@ function updateChart(filterKey) {
             return xScale(d.M_weekly);
         })
         .attr('height', barHeight)
-        .attr('fill', '#377eb8');
+        .attr('fill', '#013F94');
 
         barEnter.append('rect')
         .attr('width', function(d) {
             return xScale(d.F_weekly);
         })
         .attr('height', barHeight)
-        .attr('fill', '#f5e042');
+        .attr('fill', '#C7E6F9');
 
         barEnter.append('text')
         .attr('x', 10)
         .attr('dy', '0.93m')
         .html(function(d) {
-            return ("weekly income of males: $" + d.M_weekly + ", weekly income of females: $" + d.F_weekly);
+            return ("weekly male income: $" + d.M_weekly + ", weekly female income: $" + d.F_weekly);
         })
         .attr('class', 'hover-label')
-        .attr('color', 'pink');
+        .attr('fill', '#ffffde');
 
         barEnter.append('text')
         .attr('x', -490)
         .attr('dy', '0.93m')
+        .attr('fill', 'white')
+        .attr('font-weight', '200')
         .text(function(d) {
             return d.Occupation;
         })
